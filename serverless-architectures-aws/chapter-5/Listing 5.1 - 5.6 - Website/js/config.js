cda04461aec86065963e947613ec146b388abdc4
Yong Liu <yong.liu1@thomsonreuters.com>
2018-12-18 20:46:18 +0800

    new

--- serverless-architectures-aws/chapter-5/Listing 5.1 - 5.6 - Website/js/config.js
+++
@@ -1,7 +0,0 @@
-var configConstants = {
-	auth0: {
-		domain: 'AUTH0-DOMAIN',
-		clientId: 'AUTH0-CLIENTID'
-	},
-  apiBaseUrl: 'https://API-GATEWAY-URL/dev'
-};
--- serverless-architectures-aws/chapter-8/Listing 8.7 - 8.9 Website/js/main.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	$(document).ready(function(){
-		userController.init(configConstants);
-		videoController.init(configConstants);
-		uploadController.init(configConstants);
-	});
-}());
